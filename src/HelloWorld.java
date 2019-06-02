1676ba5e300f8e9359f53507865789068f369a79
lomeshnaik <lomeshnaik@yahoo.com>
2019-06-02 00:24:07 -0400

    new  learn java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,8 @@ import java.util.Date;
 public class HelloWorld {
     public static void main(String[] args ){
         System.out.println("Hello World, The time is:........"    + new java.util.Date());
+
+
     }
 
 }
