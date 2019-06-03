78ee621fad9faccc069aaf5373341736708d99eb
lomeshnaik <lomeshnaik@yahoo.com>
2019-06-02 22:47:43 -0400

    new  learn java

---
+++ problam_1/src/Problem_1.java
@@ -0,0 +1,10 @@
+public class Problem_1 {
+
+    public static void  main(String[] args){
+        System.out .println(" 9.2 X 4.5 - 2.5 X 3 ");
+        System.out. println("------------------------------");
+        System .out. println("     49.8 - 4.5 ");
+        System.out.println("                   =");
+
+    }
+}
