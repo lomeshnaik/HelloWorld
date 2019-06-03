67e69c2dad2098a3c26e49ab809da93287433703
lomeshnaik <lomeshnaik@yahoo.com>
2019-06-02 22:20:14 -0400

    new  learn java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,23 @@
 import java.util.Date;
+import java.io.PrintStream;
 public class HelloWorld {
     public static void main(String[] args ){
         System.out.println("Hello World, The time is:........"    + new java.util.Date());
 
+    }
+
+    public class Time {
+        public void Td(String... args ) {
+            PrintStream dt = System.out;
+            Date dt1 = new Date();
+            dt.println("hello world,  the time is:"  + dt1);
+
+              }
+
 
     }
 
-}
+    }
+
+
+
